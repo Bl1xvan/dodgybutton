1028f7faf6cbbd08a8e830f3607b8bce8a940e3f
Bl1xvan <cflavien1@my.brookdalecc.edu>
2021-08-27 19:31:01 -0400

    made face and button more responsive

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const gridItem = document.getElementById("grid-item");
+const buttonPosition = document.getElementById("button-position");
+
+
+
+console.log(document.body.getBoundingClientRect());
 
 gridItem.addEventListener("mouseover", function(){
-    console.log(this.children[0].children[0]);
-    this.children[0].children[0].setAttribute("src", "./images/evilface.png");
+    const angel = this.children[0].children[0];
+    angel.setAttribute("src", "./images/evilface.png");
+    this.style.top = Math.abs(movementY(20, 400))+`px`;
+    this.style.left = Math.abs(movmentX(100, 1400))+`px`;
+    this.style.bottom = Math.abs(movementY(20, 400))+`px`;
+    this.style.right = Math.abs(movmentX(100, 1400))+`px` ;
+
+    const gridPosValue = gridItem.getBoundingClientRect();
+    buttonPosition.innerHTML = "X = " +gridPosValue.x + " Y = " + gridPosValue.y;
 })
+
+function movmentX(min, max){
+    return Math.floor(Math.random() * (min - max + 1)) + min;
+}
+
+function movementY(min, max){
+    return Math.floor(Math.random() * (min - max + 1)) + min;
+}
