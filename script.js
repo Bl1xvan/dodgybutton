b8dfb1bbcb246a7e7e4d6b8ae75fa120d39c7472
Bl1xvan <cflavien1@my.brookdalecc.edu>
2021-08-27 19:41:59 -0400

    added debug file

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
 const gridItem = document.getElementById("grid-item");
-const buttonPosition = document.getElementById("button-position");
+const sizeRecord = document.getElementById("size-record");
+const windowBox = document.body;
+const clickMe = document.getElementById("click-me");
 
+windowBox.addEventListener("resize", function(){
+    let windowSize = windowBox.getBoundingClientRect();
+    let gridSize = gridItem.getBoundingClientRect();
 
+    console.log(windowSize.x);
+    console.log(gridSize.x);
 
-console.log(document.body.getBoundingClientRect());
-
-gridItem.addEventListener("mouseover", function(){
-    const angel = this.children[0].children[0];
-    angel.setAttribute("src", "./images/evilface.png");
-    this.style.top = Math.abs(movementY(20, 400))+`px`;
-    this.style.left = Math.abs(movmentX(100, 1400))+`px`;
-    this.style.bottom = Math.abs(movementY(20, 400))+`px`;
-    this.style.right = Math.abs(movmentX(100, 1400))+`px` ;
-
-    const gridPosValue = gridItem.getBoundingClientRect();
-    buttonPosition.innerHTML = "X = " +gridPosValue.x + " Y = " + gridPosValue.y;
 })
-
-function movmentX(min, max){
-    return Math.floor(Math.random() * (min - max + 1)) + min;
-}
-
-function movementY(min, max){
-    return Math.floor(Math.random() * (min - max + 1)) + min;
-}
